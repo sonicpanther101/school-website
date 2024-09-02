c7f817d96fad7e937928c729585c831ad1f7667c
sonicpanther101 <sonicpanther101@gmail.com>
2024-09-02 21:13:53 +1200

    somework on nav bar

--- layouts/head.tsx
+++ layouts/head.tsx
@@ -8,6 +8,8 @@ import {
   NavbarMenuItem
 } from "@nextui-org/navbar";
 
+import { ThemeSwitch } from "@/components/theme-switch";
+
 import Link from "next/link";
 import { Button, ButtonGroup } from "@nextui-org/button";
 
@@ -29,8 +31,13 @@ export const Head = () => {
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <ButtonGroup>
           <NavbarItem>
-            <Link color="foreground" href="#" className="p-4">
-              Home
+            <Link color="foreground" href="#" className="p-4 m-xl">
+              <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl p-sm">
+                  Home
+                </span>
+              </button>
             </Link>
           </NavbarItem>
           <NavbarItem>
@@ -54,6 +61,9 @@ export const Head = () => {
           <Button as={Link} color="primary" href="#" variant="flat">
             Sign Up
           </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <ThemeSwitch className="ml-xl"/>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Head } from "@/layouts/head";
 import { Footer } from "@/layouts/footer";
 import { useRef } from "react";
+import { Image } from "@nextui-org/image";
 
 import Autoplay from "embla-carousel-autoplay"
 
@@ -13,6 +14,22 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 
+
+if (typeof window !== 'undefined') {
+  const observer = new MutationObserver(() => {
+    if (document.documentElement.getAttribute('data-theme') === "dark") {
+      document.documentElement.classList.add("dark");
+    } else if (document.documentElement.getAttribute('data-theme') === "light") {
+      document.documentElement.classList.remove("dark");
+    }
+  });
+
+  observer.observe(document.documentElement, {
+    attributes: true,
+    attributeFilter: ['data-theme'],
+  });
+}
+
 export default function IndexPage() {
 
   const plugin = useRef(
@@ -22,6 +39,22 @@ export default function IndexPage() {
   return (
     <div>
       <Head />
+
+      <Image
+        width="100%"
+        alt="Maunga skiing"
+        src="https://imgs.search.brave.com/tDOmKAC5oVW5aY3l7Vovuom5EARlVQ94U_8d1s09gZM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTUw/NjM4NjcxL3Bob3Rv/L3NraWVyLXNraWlu/Zy1vbi1zbm93eS1z/bG9wZS5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9RnpNXzJG/dGRsNUItQXl5Qkx2/OXRVM1lDaFhPVDZq/dzRnYl9Pb2JwbUhB/dz0"
+      />
+      <Image
+        width="100%"
+        alt="Maunga skiing"
+        src="https://imgs.search.brave.com/tDOmKAC5oVW5aY3l7Vovuom5EARlVQ94U_8d1s09gZM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTUw/NjM4NjcxL3Bob3Rv/L3NraWVyLXNraWlu/Zy1vbi1zbm93eS1z/bG9wZS5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9RnpNXzJG/dGRsNUItQXl5Qkx2/OXRVM1lDaFhPVDZq/dzRnYl9Pb2JwbUhB/dz0"
+      />
+      <Image
+        width="100%"
+        alt="Maunga skiing"
+        src="https://imgs.search.brave.com/tDOmKAC5oVW5aY3l7Vovuom5EARlVQ94U_8d1s09gZM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTUw/NjM4NjcxL3Bob3Rv/L3NraWVyLXNraWlu/Zy1vbi1zbm93eS1z/bG9wZS5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9RnpNXzJG/dGRsNUItQXl5Qkx2/OXRVM1lDaFhPVDZq/dzRnYl9Pb2JwbUhB/dz0"
+      />
       
       <Carousel
         plugins={[plugin.current]}
@@ -32,7 +65,7 @@ export default function IndexPage() {
         <CarouselContent>
           {Array.from({ length: 5 }).map((_, index) => (
             <CarouselItem key={index}>
-              <div className="p-1 w-screen">
+              <div className="p-1 w-screen h-xl">
                 <Card>
                   <CardContent className="flex aspect-square items-center justify-center p-6">
                     <span className="text-4xl font-semibold">{index + 1}</span>
