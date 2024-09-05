b306d1082bff119cdd953aecf146e15f23829f5d
sonicpanther101 <sonicpanther101@gmail.com>
2024-09-05 12:53:08 +1200

    added unstyled inputs to form and found autocomplete attribute

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -2,6 +2,12 @@ import { Head } from "@/layouts/head";
 import { Footer } from "@/layouts/footer";
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/react";
+
+import { useState } from "react";
+
+import { EyeFilledIcon } from "@/layouts/EyeFilledIcon";
+import { EyeSlashFilledIcon } from "@/layouts/EyeSlashFilledIcon";
 
 if (typeof window !== 'undefined') {
     const observer = new MutationObserver(() => {
@@ -19,25 +25,83 @@ if (typeof window !== 'undefined') {
 }
 
 function submitRegistration() {
-    return 0
+    
 }
 
 
 export default function IndexPage() {
 
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => setIsVisible(!isVisible);
+
     return (
         <div className="overflow-hidden">
             <Head />
 
-            <Card>
+            <Card className="w-[90%] m-[5%] mt-[10%] h-[70vh] flex flex-col">
                 <CardHeader>
-                    <h1 className="text-5xl font-black">Register</h1>
+                    <h1 className="text-5xl font-black m-xl">Register</h1>
                 </CardHeader>
-                <CardBody>
+                <CardBody className="m-lg">
                     <p className="text-lg">Register a new account</p>
+                    <form className="flex flex-col my-sm" onSubmit={submitRegistration} autoComplete="on">
+                        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+                            <Input
+                                type="text"
+                                label="First name"
+                                variant="bordered"
+                                placeholder="Adam"
+                                defaultValue="junior@nextui.org"
+                                onClear={() => console.log("input cleared")}
+                                className="max-w-xs"
+                            />
+                            <Input
+                                type="text"
+                                label="Last name"
+                                variant="bordered"
+                                placeholder="Beattie"
+                                defaultValue="junior@nextui.org"
+                                onClear={() => console.log("input cleared")}
+                                className="max-w-xs"
+                            />
+                        </div>
+                        <Input
+                            isClearable
+                            type="email"
+                            label="Email"
+                            variant="bordered"
+                            placeholder="Enter your email"
+                            defaultValue="junior@nextui.org"
+                            onClear={() => console.log("input cleared")}
+                            className="max-w-xs"
+                        />
+                        <Input
+                            label="Password"
+                            variant="bordered"
+                            placeholder="Enter your password"
+                            endContent={
+                                <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
+                                    {isVisible ? (
+                                        <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                    ) : (
+                                        <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                    )}
+                                </button>
+                            }
+                            type={isVisible ? "text" : "password"}
+                            className="max-w-xs"
+                        />
+                        <Button
+                            className="w-x4l"
+                            type="submit"
+                        >
+                            Sign up &rarr;
+                        </Button>
+                    </form>
                 </CardBody>
                 <CardFooter>
-                    <Button onClick={() => {submitRegistration()}}>Register</Button>
+                    <p className="text-lg">Already have an account? Too bad, I was only given a week.</p>
                 </CardFooter>
             </Card>
 
